2efde97334d2e066156c3112c873894c771beafe
DESKTOP-GHRRVG9\pc <fayselcode@gmail.com>
2023-07-04 00:38:35 +0100

    some opt

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Hero from "./components/2_hero_section/Hero";
 import AboutUs from "./components/3_aboutUs_section/AboutUs";
 import Services from "./components/4_services/Services";
-// import CartLists from "./components/5_cars_section/carLists/CarLists";
+import CartLists from "./components/5_cars_section/carLists/CarLists";
 import CarsImages from "./components/6_cars_images_section/CarsImages";
 import Testimonials from "./components/7_testimonials_section/Testimonials";
 import Sponsors from "./components/8_sponsors_section/Sponsors";
@@ -14,7 +14,7 @@ function App() {
       <Hero />
       <AboutUs />
       <Services />
-      {/* <CartLists /> */}
+      <CartLists />
       <CarsImages />
       <Testimonials />
       <Sponsors />
