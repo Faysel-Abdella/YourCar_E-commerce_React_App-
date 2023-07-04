13d029edb6a4e765b01a3484c87031a6c2ee9334
DESKTOP-GHRRVG9\pc <fayselcode@gmail.com>
2023-07-04 20:29:08 +0100

    casee

--- src/components/3_aboutUs_section/AboutUs.js
+++ src/components/3_aboutUs_section/AboutUs.js
@@ -1,7 +1,7 @@
 import classes from "./aboutUs.module.css";
 
 const AboutUs = () => {
-  const myimg = require("../../assets/aboutUsimg/abtUs.png");
+  const myimg = require("../../assets/aboutUsimg/abtus.png");
   return (
     <div className={classes["about-us-container"]} id="section2">
       <h1>ABOUT US</h1>
